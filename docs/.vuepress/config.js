08f4972df7f9f4227e0122a22c3fc146babc9174
唐钰豆豆 <doudoudzj@qq.com>
2021-08-20 11:55:22 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,6 +16,8 @@ module.exports = {
         }
     },
     head: [
+        ['meta', { name: 'author', content: 'CROGRAM' }],
+        ['meta', { name: 'copyright', content: 'CROGRAM 程江开源' }],
         ['meta', { name: 'keywords', content: 'InPanel, VPSMate, Linux, VPS, VPS Management, 开源, VPS管理面板, 开源Linux服务器管理面板' }],
         ['meta', { name: 'baidu-site-verification', content: '64Wi1MqMba' }],
         ['meta', { name: 'google-site-verification', content: 'lE5T54JZzDGH2Ougb83nRswErCNpS2fUByVdoT6lJ8w' }]
